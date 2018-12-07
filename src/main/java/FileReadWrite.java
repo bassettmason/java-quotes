880795f6357460c5ecff5ba6a3f66ee14dacad6a
Mason Bassett <bassettmason@gmail.com>
2018-12-07 09:42:34 -0800

    Lab_8_Done&Tested

--- src/main/java/FileReadWrite.java
+++ src/main/java/FileReadWrite.java
@@ -5,20 +5,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class FileReadWrite {
-    public static BufferedReader fileToReaderObject(){
-        // create the path and read the file
-        BufferedReader jsonReader;
 
-        try {
-            Path path = FileSystems.getDefault().getPath("resources", "recentquotes.json");
-            jsonReader = Files.newBufferedReader(path);
-
-        } catch (IOException e) {
-            System.out.println(e);
-            jsonReader = null; // do something better practice after base functionality established.
-
-        }
-
-        return jsonReader;
-    }
 }
+
